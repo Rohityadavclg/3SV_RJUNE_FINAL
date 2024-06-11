b1aad3be0f627085cfc6cfe8ab3ed8ab58b7080e
Rohityadavclg <rohitkyadav82@gmail.com>
2024-06-11 19:38:20 +0530

    working with payment gateways and email verification

--- server/config/razorpay.js
+++ server/config/razorpay.js
@@ -1,6 +1,18 @@
-const Razorpay = require("razorpay");
+//const Razorpay = require("razorpay");
 
-exports.instance = new Razorpay({
-	key_id: process.env.RAZORPAY_KEY,
-	key_secret: process.env.RAZORPAY_SECRET,
+// exports.instance = new Razorpay({
+// 	key_id: process.env.RAZORPAY_KEY,
+// 	key_secret: process.env.RAZORPAY_SECRET,
+// });
+ 
+
+const Razorpay = require('razorpay');
+require('dotenv').config();
+ 
+
+const razorpay = new Razorpay({
+    key_id: process.env.RAZORPAY_KEY,
+    key_secret: process.env.RAZORPAY_SECRET,
 });
+
+module.exports = razorpay;
