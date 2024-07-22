fddf5f46ca5755ea04e0e2d5051edabc22e13f3f
Rohityadavclg <rohitkyadav82@gmail.com>
2024-07-22 11:15:50 +0530

    final changes in navbar

--- src/components/Common/Navbar.jsx
+++ src/components/Common/Navbar.jsx
@@ -1,73 +1,98 @@
-import { useEffect, useState } from "react"
-import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai"
-import { BsCheckLg,BsChevronDown } from "react-icons/bs"
-import { useSelector } from "react-redux"
-import { Link, matchPath, useLocation } from "react-router-dom"
-
-import logo from "../../assets/Images/brand-logo.svg"
-import { NavbarLinks } from "../../data/navbar-links"
-import { apiConnector } from "../../services/apiConnector"
-import { categories } from "../../services/apis"
-import { ACCOUNT_TYPE } from "../../utils/constants"
-import ProfileDropdown from "../core/Auth/ProfileDropdown"
-import brandlogo from '../../assets/Logo/brand-logo.png'
+// // // src/components/Common/Navbar.js
+import { useEffect, useState } from "react";
+import { AiOutlineMenu, AiOutlineShoppingCart } from "react-icons/ai";
+import { BsChevronDown } from "react-icons/bs";
+import { useSelector } from "react-redux";
+import { Link, matchPath, useLocation } from "react-router-dom";
+import { NavbarLinks } from "../../data/navbar-links";
+import { apiConnector } from "../../services/apiConnector";
+import { categories } from "../../services/apis";
+import { ACCOUNT_TYPE } from "../../utils/constants";
+import ProfileDropdown from "../core/Auth/ProfileDropdown";
+import brandlogo from '../../assets/Logo/brand-logo.png';
+import DarkModeToggler from "../Common/DarkModeToggler";
+const serviceLinks = [
+  {
+    title: "Consulting",
+    link: "/service/consulting"
+  },
+  {
+    title: "Coorporate Training",
+    link: "/service/corporate-training"
+  },
+  {
+    title: "Entrepreneurship",
+    link: "/service/entrepreneship"
+  },
+  {
+    title: "Coorporate Resource Center",
+    link: "/service/co-re-center"
+  },
+  {
+    title: "Projects",
+    link: "/service/project"
+  },
+  {
+    title: "Research and Development",
+    link: "/service/research-and-development"
+  },
+];
 function Navbar() {
-  const { token } = useSelector((state) => state.auth)
-  const { user } = useSelector((state) => state.profile)
-  const { totalItems } = useSelector((state) => state.cart)
-  const location = useLocation()
+  const { token } = useSelector((state) => state.auth);
+  const { user } = useSelector((state) => state.profile);
+  const { totalItems } = useSelector((state) => state.cart);
+  const isDarkMode = useSelector((state) => state.ui.isDarkMode);
+  const location = useLocation();
 
-  const [subLinks, setSubLinks] = useState([])
-  const [loading, setLoading] = useState(false)
+  const [subLinks, setSubLinks] = useState([]);
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    ;(async () => {
-      setLoading(true)
+    (async () => {
+      setLoading(true);
       try {
-        const res = await apiConnector("GET", categories.CATEGORIES_API)
-        setSubLinks(res.data.data)
+        const res = await apiConnector("GET", categories.CATEGORIES_API);
+        setSubLinks(res.data.data);
       } catch (error) {
-        console.log("Could not fetch Categories.", error)
+        console.log("Could not fetch Categories.", error);
       }
-      setLoading(false)
-    })()
-  }, [])
-
-  // console.log("sub links", subLinks)
+      setLoading(false);
+    })();
+  }, []);
 
   const matchRoute = (route) => {
-    return matchPath({ path: route }, location.pathname)
-  }
+    return matchPath({ path: route }, location.pathname);
+  };
 
   return (
     <div
-      className={`flex h-14 items-center justify-center border-b-[1px] border-b-richblack-700 ${
-        location.pathname !== "/" ? "bg-richblack-800" : ""
+      className={`flex h-14 items-center justify-center border-b-[1px] ${
+        location.pathname !== "/" ? (isDarkMode ? "bg-richblack-800" : "bg-white") : ""
       } transition-all duration-200`}
     >
       <div className="flex w-11/12 max-w-maxContent items-center justify-between">
         {/* Logo */}
         <Link to="/">
-          <img src={brandlogo} alt="Logo" width={160} height={32} color="black" loading="lazy"/>
+          <img src={brandlogo} alt="Logo" width={160} height={3} color="black" loading="lazy"/>
         </Link>
         {/* Navigation links */}
         <nav className="hidden md:block">
-          <ul className="flex gap-x-6 text-richblack-25">
+          <ul className="flex gap-x-6">
             {NavbarLinks.map((link, index) => (
               <li key={index}>
-                {link.title === "Catalog" ? (
+                {link.title === "Course" ? (
                   <>
                     <div
                       className={`group relative flex cursor-pointer items-center gap-1 ${
                         matchRoute("/catalog/:catalogName")
-                          ? "text-yellow-25"
-                          : "text-richblack-25"
+                          ? "text-yellow-10"
+                          : (isDarkMode ? "text-richblack-25" : "text-black")
                       }`}
                     >
                       <p>{link.title}</p>
                       <BsChevronDown />
-                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-richblack-5 p-4 text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
-                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-richblack-5"></div>
+                      <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-white dark:bg-richblack-5 p-4 dark:text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                        <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-white dark:bg-richblack-5"></div>
                         {loading ? (
                           <p className="text-center">Loading...</p>
                         ) : subLinks?.length ? (
@@ -82,7 +107,7 @@ function Navbar() {
                                     .split(" ")
                                     .join("-")
                                     .toLowerCase()}`}
-                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50"
+                                  className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50 dark:hover:bg-richblack-300"
                                   key={i}
                                 >
                                   <p>{subLink.name}</p>
@@ -95,25 +120,43 @@ function Navbar() {
                       </div>
                     </div>
                   </>
-                ) : (
-                  <Link to={link?.path}>
-                    <p
-                      className={`${
-                        matchRoute(link?.path)
-                          ? "text-yellow-25"
-                          : "text-richblack-25"
-                      }`}
-                    >
-                      {link.title}
-                    </p>
-                  </Link>
-                )}
-              </li>
-            ))}
+                ) : link.title === "Services" ? (
+                      <div className='group relative flex cursor-pointer items-center gap-1'>
+                        <p>{link.title}</p>
+                        <BsChevronDown />
+
+                        <div className="invisible absolute left-[50%] top-[50%] z-[1000] flex w-[200px] translate-x-[-50%] translate-y-[3em] flex-col rounded-lg bg-white dark:bg-richblack-5 p-4 dark:text-richblack-900 opacity-0 transition-all duration-150 group-hover:visible group-hover:translate-y-[1.65em] group-hover:opacity-100 lg:w-[300px]">
+                          <div className="absolute left-[50%] top-0 -z-10 h-6 w-6 translate-x-[80%] translate-y-[-40%] rotate-45 select-none rounded bg-white dark:bg-richblack-5"></div>
+                        {
+                            serviceLinks.map((service, index) => (
+                              <Link to={`${service.link}`} 
+                              className="rounded-lg bg-transparent py-4 pl-4 hover:bg-richblack-50 dark:hover:bg-richblack-50"
+                              key={index}>
+                                <p>{service.title}</p>
+                              </Link>
+                            ))
+                          }
+                        </div>
+                      </div>
+                    ) : (
+                      <Link to={link?.path}>
+                        <p className={`${matchRoute("/services/:serviceName") ? "text-yellow-25" : "text-richblack-25"}`}>
+                          {link.title}
+                        </p>
+                      </Link>
+                    )
+                  }
+                </li>
+              ))
+            }
           </ul>
         </nav>
+
         {/* Login / Signup / Dashboard */}
         <div className="hidden items-center gap-x-4 md:flex">
+          <div className='theme-Switch-container'>
+            <DarkModeToggler />
+          </div>
           {user && user?.accountType !== ACCOUNT_TYPE.INSTRUCTOR && (
             <Link to="/dashboard/cart" className="relative">
               <AiOutlineShoppingCart className="text-2xl text-richblack-100" />
@@ -145,7 +188,6 @@ function Navbar() {
         </button>
       </div>
     </div>
-  )
+  );
 }
-
-export default Navbar
+export default Navbar; 
